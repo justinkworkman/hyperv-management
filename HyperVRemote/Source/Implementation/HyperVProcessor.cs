b649ba7e6b6c02552d41f547c1eef47b339dc5c3
justinkworkman <88411645+justinkworkman@users.noreply.github.com>
2021-08-20 22:18:39 -0500

    Added class and method

---
+++ HyperVRemote/Source/Implementation/HyperVProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using HyperVRemote.Source.Interface;
+
+namespace HyperVRemote.Source.Implementation
+{
+    public class HyperVProcessor : IHyperVProcessor
+    {
+        private readonly ManagementObject _rawCpu;
+        public HyperVProcessor(ManagementObject rawCpu)
+        {
+            _rawCpu = rawCpu;
+        }
+        public int CPUUsage { get; set; }
+        public Guid SystemName { get; set; }
+    }
+}
